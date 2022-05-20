1cbd0405b9ca3edf52fa5ac13ff97f058e78790a
Ganapathi T <ganapathi423@gmail.com>
2022-05-20 14:10:05 +0530

    Apikey store in map

--- RestProject/src/main/java/com/maths/io/controller/RestApis.java
+++ RestProject/src/main/java/com/maths/io/controller/RestApis.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,53 +18,62 @@ public class RestApis {
 
 	@RequestMapping("/api")
 	@ResponseBody
-	public String getApikey(
-			@RequestParam("uName") String uName,
-			@RequestParam("pass") String pass
-			) throws Exception {
-		
-		String sql = "SELECT * FROM boodskap";
+	public Map<String, String> getApikey(@RequestParam("uName") String uName, @RequestParam("uPass") String uPass) throws Exception {
 
-		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-				+ "0123456789" + "abcdefghijklmnopqrstuvxyz";
+		String sql = "SELECT * FROM bsl";
+
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
 
 		StringBuilder sb = new StringBuilder();
 
 		Random random = new Random();
 
-		int length = 16;
-		
+		int length = 12;
+
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bds", "root", "example");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/boodskap", "root", "example");
 
 		Statement st = con.createStatement();
 
 		ResultSet rs = st.executeQuery(sql);
 		
-		while(rs.next()) {
-			
-			if(rs.getString(1).contains(uName)&&rs.getString(2).contains(pass)) {
-			
-				for(int i = 0; i < length; i++) {
+		Map<String, String> key = new HashMap<>();
+
+		while (rs.next()) {
+
+			if (rs.getString(1).contains(uName) && rs.getString(2).contains(uPass)) {
+
+				for (int i = 1; i < length; i++) {
 
 					int index = random.nextInt(alphabet.length());
-					
+
 					char randomChar = alphabet.charAt(index);
+
+					//Apikey
 					
 					sb.append(randomChar);
+					
+					//Token
 
+//					if (i % 5 == 0)
+//						sb.append("-");
+//					else
+//						sb.append(randomChar);
+					
+					
+					key.put("Apikey", sb.toString());
 				}
-				
-			}else {
-				return "woring input";
+
+			} else {
+				Map<String, String> key1 = new HashMap<>();
+				return key1;
+				//return "woring input";
 			}
 
 		}
 
-		String Apikey = sb.toString();
-	
-		return	"ApiKey = > "+Apikey;
+		return key;
 
 	}
 
