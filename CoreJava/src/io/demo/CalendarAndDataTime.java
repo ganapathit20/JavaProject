cdaa6fcdcc1b76a4b5dae1127aa76acf58d3fcb3
Ganapathi T <ganapathi423@gmail.com>
2022-06-01 10:50:25 +0530

    Java Calendar and Date Time

---
+++ CoreJava/src/io/demo/CalendarAndDataTime.java
@@ -0,0 +1,64 @@
+package io.demo;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class CalendarAndDataTime {
+
+	static public void  main(String ...strings ) throws Exception {
+
+		Map<String, String> sysPro = System.getenv();
+
+		Map<Object, Object> sysPro1 =	System.getProperties();
+
+		System.out.println(System.getProperties());
+
+		System.out.println(System.getenv("HOME"));
+
+		//String java = System.getProperty("java.specification.version");
+
+		System.out.println(System.currentTimeMillis());
+
+		for(Entry<Object, Object> data : sysPro1.entrySet()) {
+
+			//	System.out.println("Key: " + data.getKey() + " <===> Value: " +sysPro.values());
+
+		}
+
+		LocalDateTime time = LocalDateTime.now();
+		System.out.println(time);
+
+		LocalTime time1 = LocalTime.now();
+		System.out.println(time1);
+
+		LocalDate date1 = LocalDate.now();
+		System.out.println(date1);
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm:ss");
+		String def = time.format(dtf);
+		System.out.println(def);
+
+
+		Calendar calendar = Calendar.getInstance();  
+		    
+		   System.out.println("At present Calendar's Day: " + calendar.get(Calendar.DATE)); 
+		   System.out.println("At present Calendar's Month: " + calendar.get(Calendar.MONTH));
+		   System.out.println("At present Calendar's Year: " + calendar.get(Calendar.YEAR)); 
+		   
+		System.out.println("The current date is : " + calendar.getTime());  
+		calendar.add(Calendar.DATE, -15);  
+		System.out.println("15 days ago: " + calendar.getTime());  
+		calendar.add(Calendar.MONTH, 4);  
+		System.out.println("4 months later: " + calendar.getTime());  
+		calendar.add(Calendar.YEAR, 2);  
+		System.out.println("2 years later: " + calendar.getTime());  
+
+
+
+
+	}
+}
--- storage-mysql/src/main/java/io/mysq/querys/Sql.java
+++ storage-mysql/src/main/java/io/mysq/querys/Sql.java
@@ -12,12 +12,12 @@ public class Sql {
 		
 		try {
 			
-	String sql = "CREATE TABLE BSL ( PersonID int, Name varchar(255), Address varchar(255), City varchar(255), Roll varchar(255));";
+	String sql = "CREATE TABLE BSL ( uName varchar(255), uPass varchar(255) );";
 	
 	
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/boodskap", "root", "example");
+		Connection con = DriverManager.getConnection("jdbc:mysql://admin.c0f25kuqwmed.us-west-1.rds.amazonaws.com:3306/admin", "admin", "admin123");
 		
 		System.out.println("connected...");
 		
