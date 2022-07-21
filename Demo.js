aa1647c0f2c776b49e4fe414c8343c758d4280f6
Ganapathi T <ganapathi423@gmail.com>
2022-07-21 15:59:14 +0530

    delete fs add

--- Demo.js
+++ Demo.js
@@ -40,21 +40,33 @@ fs.readFile("cal.js", "UTF8", (error, data)=>{
 })
 
 //write files 
-// it is create new file if does not exited 
-// fs.writeFile("cal1.js", "console.info(\"new File created\")", function(error){
-//     if(error)
-//     throw error
+//it is create new file if does not exited 
+//readFile 3 args needs 1.filePath, 2. content for the file what you write, 3. callback function
+//callback function needs 2 args 1. error, 2. data(give any name)
+// callback funcrion use 2ways 1. function(){}, 2. ()=>{}
+fs.writeFile("cal1.js", "console.info(\"new File created\")", function(error){
+    if(error)
+    throw error
 
-//     console.info("done...")
+    console.info("create done...")
 
-// })
+})
 
 
-// it is append data to the same file if there
-// fs.appendFile("cal.js", "console.info(\"hi\")", function(error){
-//     if(error)
-//     throw error
+//it is append data to the same file if there
+fs.appendFile("cal.js", "console.info(\"hi\")", function(error){
+    if(error)
+    throw error
 
-//     console.info("done...")
+    console.info("append done...")
 
-// })
+})
+
+//it is delete data  from file 
+fs.unlink("cal1.js", function(error){
+    if(error)
+     throw error
+
+    console.info("deleted done...")
+
+})
--- cal.js
+++ cal.js
@@ -13,3 +13,4 @@ exports.substraction = substraction; // type 2 for moudle export
 exports.multiplication = function substraction(fValue, sValue){ // type 3 for moudle export
     return fValue * sValue;
 }
+
